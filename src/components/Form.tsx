07250540dbecddeebdab4c8f5c4a5eb7ee7c0d9d
peninah98 <peninangizwenayo@gmail.com>
2023-11-09 10:23:44 +0200

    refactor: changing form background color

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from "react";
 export default function Form(): ReactElement {
   return (
     <div className="h-screen flex justify-center items-center">
-      <form className="bg-blue-800 p-10 shadow-2xl rounded-md">
+      <form className="bg-green-800 p-10 shadow-2xl rounded-md">
         <h1 className="text-gray-100 font-semibold text-3xl pb-10">Todo App</h1>
         <input
           type="text"
