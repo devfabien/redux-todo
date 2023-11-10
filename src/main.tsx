577eab27af7ee6ee23589942c24b8945b8f513db
devfabien <ish001fabi@gmail.com>
2023-11-10 14:33:06 +0200

    feat: creating the store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./redux/todo.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const store = configureStore({
+  reducer: {
+    todo: todoReducer,
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
