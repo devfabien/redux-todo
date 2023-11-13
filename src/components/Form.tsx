bf7187edcb1826e805ca06222a83e33d4171c1a1
devfabien <ish001fabi@gmail.com>
2023-11-13 12:44:41 +0200

    feat: adding the function to handle change and handle submit of the form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,36 @@
-import { ReactElement, useState } from "react";
+import { ChangeEvent, FormEvent, ReactElement, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/todo";
 
 export default function Form(): ReactElement {
+  const dispatch = useDispatch();
   const [input, setInput] = useState("");
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setInput(event.target.value);
+  }
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (input !== "") {
+      dispatch(addTodo(input));
+    }
+    setInput("");
+  }
+
   return (
-    <div className="h-screen flex justify-center items-center">
-      <form className="bg-blue-800 p-10 shadow-2xl rounded-md">
-        <h1 className="text-gray-100 font-semibold text-3xl pb-10">Todo App</h1>
+    <div className="h-screen flex flex-col w-1/2 mx-auto justify-center items-center">
+      <h1 className="text-gray-300 font-semibold text-5xl pb-10">Todo App</h1>
+      <form
+        className="bg-blue-800 p-10 shadow-2xl flex items-center rounded-md"
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           name="todoTask"
           placeholder="Todo task..."
           value={input}
-          onChange={(event) => setInput(event.target.value)}
+          onChange={handleChange}
           className="outline-none py-3 px-6 mx-3 text-gray-800"
         />
         <button className="mx-4 text-lg bg-gray-500 p-3 rounded-full hover:bg-gray-400 hover:font-semibold text-gray-100">
